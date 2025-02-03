49f918787305009f2639e18fb261e53c22d98a04
“sungjunkim” <kis07150@naver.com>
2025-02-04 00:05:53 +0900

    변수 미니퀘스트 완료

---
+++ js_miniquests/variable.js
@@ -0,0 +1,23 @@
+const name = "제이슨"
+let age = 28
+let height = 180.1
+
+// 1번
+console.log(name)
+console.log(age)
+console.log(height)
+
+// 2번
+console.log(name, age, height)
+
+// 과제
+const create = "1. 작성"
+const read = "2. 조회"
+const update = "3. 수정"
+const del = "4. 삭제"
+const add_func = "5. 추가기능"
+const terminate = "6. 종료"
+
+console.log(create, read, update, del, add_func, terminate)
+
+
